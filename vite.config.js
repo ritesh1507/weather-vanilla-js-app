40820980f640e9d3da2b1985f0248a404869d176
ritesh <ritesharah@gmail.com>
2024-06-14 18:23:31 +0530

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import {defineConfig}  from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    base:'/weather-vanilla-js-app/'
+    base:'/weather-vanilla-js-app/',
+    plugins:[vue()]
 })
